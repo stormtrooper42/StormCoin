afe996f96853313d0f0abb69286020930207cb4d
StormTrooper42 <faurest.lupine.pro@gmail.com>
2016-03-19 21:31:22 +0100

    Remove directory

--- build/release/external/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dream/StormCoin/StormCoin/external/gtest
-# Build directory: /home/dream/StormCoin/StormCoin/build/release/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/release/external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dream/StormCoin/StormCoin/external/miniupnpc
-# Build directory: /home/dream/StormCoin/StormCoin/build/release/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
